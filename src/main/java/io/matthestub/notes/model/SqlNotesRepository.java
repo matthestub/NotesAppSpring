df51330d8327a402f7bcb8ef7339b1216f6aefb8
matthestub <mmizeracki22@gmail.com>
2023-05-16 23:10:27 +0200

    added properties/yaml and my own ConfigurationProperties

--- src/main/java/io/matthestub/notes/model/SqlNotesRepository.java
+++ src/main/java/io/matthestub/notes/model/SqlNotesRepository.java
@@ -5,19 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@RepositoryRestResource
+@Repository
 public interface NotesRepository extends JpaRepository<Note, Integer> {
-
-    @Override
-    @RestResource(exported = false)
-    void deleteById(Integer integer);
-
-    @Override
-    @RestResource(exported = false)
-    void delete(Note entity);
 
     @RestResource(path = "done", rel = "done")
     List<Note> findByDone(@Param("state") boolean state);
